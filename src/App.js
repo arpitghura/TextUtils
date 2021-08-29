a1d8900b9c9c9c3035e093e6773a42d88b694573
Arpit Ghura <ghurarpit110@gmail.com>
2021-08-29 17:26:26 +0530

    TextUtils App Updated

--- src/App.js
+++ src/App.js
@@ -1,30 +1,60 @@
 import './App.css';
-import About from './Components/About';
+// import About from './Components/About';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
 import React, { useState } from 'react'
+import Alert from './Components/Alert';
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route
+// } from "react-router-dom";
 
 
 function App() {
   const [mode, setMode] = useState('light'); // whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
 
+  const showAlert = (message, type)=>{
+      setAlert({
+        msg : message,
+    	  type : type
+      })
+      setTimeout(() => {
+          setAlert(null)
+      }, 1500);
+  }
   const toggleMode = () =>{
     if (mode === 'light') {
       setMode('dark');
-      document.body.style.backgroundColor = '#042743'
+      document.body.style.backgroundColor = '#042743';
+      showAlert("Dark Mode has been enabled","success");
+      // document.title = 'TextUtils - Dark Mode'; // Change Title Dynamically
     } else {
       setMode('light');
-      document.body.style.backgroundColor = 'white'
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light Mode has been enabled","success");
     }
   }
 
   return (
     <>
+    {/* <Router> */}
     <Navbar mode={mode} toggleMode={toggleMode} />
+    <Alert alert={alert}/>
     <div className="container my-3">
-    <TextForm heading="Enter Text Below to Analyse" mode={mode}/>
-    {/* <About/> */}
+    {/* /users --> Component 1
+      	/users/home --> Component 2 */}
+    {/* <Switch> */}
+          {/* <Route exact path="/about"> 
+            <About />
+          </Route> */}
+          {/* <Route exact path="/"> */}
+          <TextForm showAlert={showAlert} heading="Enter Text Below to Analyse" mode={mode}/>
+          {/* </Route> */}
+    {/* </Switch> */}
     </div>
+    {/* </Router> */}
     </>
   );
 }
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -5,23 +5,23 @@ export default function TextForm(props) {
         // console.log("Uppercase was clicked " + text);
         let newText = text.toUpperCase();
         setText(newText);
+        props.showAlert("Converted to Upper Case", "success");
     }
     const handleDownClick = () => {
         let newText = text.toLowerCase();
         setText(newText);
+        props.showAlert("Converted to Lower Case", "success");
     }
     const handleClearClick = () => {
         let newText = '';
         setText(newText);
+        props.showAlert("Text Cleared", "success");
     }
     const handleCopy = () => {
         let text = document.getElementById("mybox");
         text.select();
         navigator.clipboard.writeText(text.value);
-        document.getElementById("copy-success-msg").style.display = 'block';
-        setTimeout(() => {
-            document.getElementById("copy-success-msg").style.display = 'none';
-        }, 1500);
+        props.showAlert("Text Copied to Clipboard", "success");
     }
     // const handleCapitalClick = () => {
     //     // let str = text.toString();
@@ -44,7 +44,7 @@ export default function TextForm(props) {
         <div className="container" style={{color: props.mode === 'dark'?'white':'black'}}>
             <h2>{props.heading}</h2>
             <div className="mb-3">
-            <textarea className="form-control" value ={text} onChange={handleOnChange} style={{backgroundColor: props.mode === 'dark'?'grey':'white',color: props.mode === 'dark'?'white':'black'}} id="mybox" rows="8"></textarea>
+            <textarea className="form-control" value ={text} onChange={handleOnChange} style={{backgroundColor: props.mode === 'dark'?'#5566':'white',color: props.mode === 'dark'?'white':'black'}} id="mybox" rows="8"></textarea>
             </div>
             <button className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert To Uppercase</button>
             <button className="btn btn-primary mx-1 my-1" onClick={handleDownClick}>Convert To Lowercase</button>
@@ -52,10 +52,6 @@ export default function TextForm(props) {
             <button className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy Text</button>
         </div>
         <div className="container my-3" style={{color: props.mode === 'dark'?'white':'black'}}>
-        {/* Copy to clipboard Message  */}
-        <div class="alert alert-success alert-dismissible fade show" id="copy-success-msg" role="alert" style = {{display:'none'}}>Copied to Clipboard
-        {/* ------- */}
-        </div>
             <h1>Your Text Summary</h1>
             <p>{text.length} Characters and {text.split(" ").length} Words</p>
             <p>Reading Time: {0.008 * text.split(" ").length} Minutes</p>
